87b8d640f382320b561c5ac44d9b984e25d99de7
JinMyeong Choi <ymo6538@gmail.com>
2026-03-03 13:33:37 +0900

    test: Vitest + Testing Library + jsdom 테스트 인프라 설정

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist', '.vite'] },
+  { ignores: ['dist', '.vite', 'coverage'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
