d82123734cf14d7472d47d029906998dbd35248e
JinMyeong Choi <ymo6538@gmail.com>
2026-03-12 11:43:01 +0900

    feat: 데스크톱 전용 뷰포트 가드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import LobbyPage from './pages/lobby/LobbyPage'
 import NicknameSetupPage from './pages/NicknameSetupPage'
 import MyPage from './pages/MyPage'
 import WaitingRoomPage from './pages/waiting-room/WaitingRoomPage'
+import { DesktopViewportGuard } from './components/DesktopViewportGuard'
 
 // 앱 전체 페이지 라우팅 테이블 정의
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/nickname-setup" element={<NicknameSetupPage />} />
-      <Route path="/my-page" element={<MyPage />} />
-      <Route path="/game/:gameId" element={<GamePage />} />
-      <Route path="/lobby" element={<LobbyPage />} />
-      <Route path="/rooms/:roomId" element={<WaitingRoomPage />} />
-    </Routes>
+    <DesktopViewportGuard>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/nickname-setup" element={<NicknameSetupPage />} />
+        <Route path="/my-page" element={<MyPage />} />
+        <Route path="/game/:gameId" element={<GamePage />} />
+        <Route path="/lobby" element={<LobbyPage />} />
+        <Route path="/rooms/:roomId" element={<WaitingRoomPage />} />
+      </Routes>
+    </DesktopViewportGuard>
   )
 }
 
