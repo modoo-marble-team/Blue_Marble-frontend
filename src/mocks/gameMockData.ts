8c5b0bc3eed7acbeeb1d457bae6499a6536d15f9
woojaemin0106 <dnwoals19@naver.com>
2026-04-03 08:13:02 +0900

    feat: sync mock board SoT and add runtime smoke qa

--- src/mocks/gameMockData.ts
+++ src/mocks/gameMockData.ts
@@ -29,7 +29,7 @@ export const mockTiles: Tile[] = [
   },
   {
     index: 5,
-    name: '\uD3C9\uD0DD',
+    name: '\uD0DC\uBC31',
     type: 'property',
     building: 0,
     price: 30000,
@@ -37,7 +37,7 @@ export const mockTiles: Tile[] = [
   },
   {
     index: 6,
-    name: '\uC775\uC0B0',
+    name: '\uC6B8\uC0B0',
     type: 'property',
     building: 0,
     price: 30000,
@@ -72,7 +72,7 @@ export const mockTiles: Tile[] = [
   },
   {
     index: 13,
-    name: '\uCCAD\uC6D0',
+    name: '\uCC3D\uC6D0',
     type: 'property',
     building: 0,
     price: 50000,
@@ -80,7 +80,7 @@ export const mockTiles: Tile[] = [
   },
   {
     index: 14,
-    name: '\uC6B8\uC0B0',
+    name: '\uC775\uC0B0',
     type: 'property',
     building: 0,
     price: 70000,
@@ -91,7 +91,7 @@ export const mockTiles: Tile[] = [
     name: '\uBD80\uC0B0',
     type: 'property',
     building: 0,
-    price: 170000,
+    price: 150000,
     color: '#EF5350',
   },
   { index: 16, name: '\uC5EC\uD589', type: 'travel', building: 0 },
@@ -100,7 +100,7 @@ export const mockTiles: Tile[] = [
     name: '\uC81C\uC8FC',
     type: 'property',
     building: 0,
-    price: 170000,
+    price: 150000,
     color: '#42A5F5',
   },
   {
@@ -138,7 +138,7 @@ export const mockTiles: Tile[] = [
   },
   {
     index: 23,
-    name: '\uC6D0\uC8FC',
+    name: '\uC804\uC8FC',
     type: 'property',
     building: 0,
     price: 50000,
@@ -189,7 +189,7 @@ export const mockTiles: Tile[] = [
     name: '\uC11C\uC6B8',
     type: 'property',
     building: 0,
-    price: 170000,
+    price: 150000,
     color: '#FF7043',
   },
 ]
